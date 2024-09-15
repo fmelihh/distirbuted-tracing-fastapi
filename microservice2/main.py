e1c4b7c4ea46be632df4407361e2fd21f22a5437
fmelihh <furkanmelihercan.98@gmail.com>
2024-09-15 20:54:31 +0300

    feat: added error counter and request counter to the microservice 1

--- microservice2/main.py
+++ microservice2/main.py
@@ -19,24 +19,30 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcess
 
 app = FastAPI()
 
-resource = Resource(attributes={SERVICE_NAME: "microservice2"})
-
-
-prometheus_client.start_http_server(port=9090, addr="localhost")
-
-reader = PrometheusMetricReader()
-provider = MeterProvider(resource=resource, metric_readers=[reader])
-metrics.set_meter_provider(provider)
-
 trace.set_tracer_provider(TracerProvider())
 trace_provider: TracerProvider = trace.get_tracer_provider()
 trace_provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
 
 tracer = trace.get_tracer(__name__)
 
+resource = Resource(attributes={SERVICE_NAME: "microservice2"})
+
+prometheus_client.start_http_server(port=8003)
+reader = PrometheusMetricReader()
+provider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(provider)
+
+meter = provider.get_meter(__name__)
+request_counter = meter.create_counter(
+    "request_counter", unit="1", description="Number of requests sent to microservice1"
+)
+error_counter = meter.create_counter(
+    "error_count", unit="1", description="Number of errors encountered during microservice1"
+)
 
 @app.get("/send-user")
 async def add_user(username: str, age: int):
+    request_counter.add(1)
     with tracer.start_as_current_span("microservice2_span"):
         ctx = baggage.set_baggage("hello", "world")
 
@@ -44,15 +50,18 @@ async def add_user(username: str, age: int):
         W3CBaggagePropagator().inject(headers, ctx)
         TraceContextTextMapPropagator().inject(headers, ctx)
 
-        print(headers)
-        requests.post(
-            "http://localhost:8000/add-user",
-            headers=headers,
-            data=json.dumps({"user": {"username": username, "age": age}}),
-        )
-
-        return "hello from microservice2"
-
+        try:
+            print(headers)
+            response = requests.post(
+                "http://localhost:8000/add-user",
+                headers=headers,
+                data=json.dumps({"user": {"username": username, "age": age}}),
+            )
+            response.raise_for_status()
+            return "hello from microservice2"
+        except Exception as e:
+            error_counter.add(1)
+            return {"error": str(e)}, 500
 
 if __name__ == "__main__":
     uvicorn.run(app=f"{Path(__file__).stem}:app", host="0.0.0.0", port=8001)
